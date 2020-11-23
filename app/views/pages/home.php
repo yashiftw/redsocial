ff7423f79c2320f1cf17f7341b2ab1534f43b512
yashiftw <53416317+yashiftw@users.noreply.github.com>
2020-11-22 22:56:10 -0500

    Add files via upload

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -56,7 +56,10 @@ include_once URL_APP . '/views/custom/navbar.php';
                             </div>
                             <?php if($datosPublicacion->idusuario == $_SESSION['logueado'] ): ?>
                             <div class="acciones-publicacion-usuario">
-                                <a href="<?php echo URL_PROJECT ?>/publicaciones/eliminar/<?php echo $datosPublicacion->idpublicacion ?>" i class="fa fa-trash" aria-hidden="true"></i></a> 
+                                <a href="<?php echo URL_PROJECT ?>/publicaciones/eliminar/<?php echo $datosPublicacion->idpublicacion ?>"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></a> 
+</svg> 
                             </div>
                             <?php endif ?>
                         </div>
